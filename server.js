4811f064050270f9a4584f95d7876027f5bbb8de
Gaurav <gauravdeep8660@gmail.com>
2023-06-22 21:09:14 +0530

    sec commit

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ writeFileSync('./txt files/second.txt',`${first}`)
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(myDateTime())
-  res.write(`<html><body><h1>have a nice day<br>
+  res.write(`<html><body><h1>bla bla bla<br>
   the file content is :</h1>
   <p>${first}${second}</p>
   </body></html>`);
