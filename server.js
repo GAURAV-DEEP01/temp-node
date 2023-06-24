8d16514a5136901ed20182e38a2d5133bb22cf38
Gaurav <gauravdeep8660@gmail.com>
2023-06-24 23:43:47 +0530

    testing

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ let {readFileSync,writeFileSync} = require('fs');
 const { myDateTime } = require('./function');
 const first = readFileSync('./txt files/first.txt','utf8');
 const second = readFileSync('./txt files/second.txt','utf8');
+
 writeFileSync('./txt files/second.txt',`${first}`)
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -12,5 +13,5 @@ http.createServer(function (req, res) {
   the file content is :</h1>
   <p>${first}${second}</p>
   </body></html>`);
-  res.end('Hello World!');
-}).listen(8000);
+  res.end();
+}).listen(8008);
